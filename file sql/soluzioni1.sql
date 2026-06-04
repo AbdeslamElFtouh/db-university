dcad364947de86a9c3eefe5d04407889ce360413
Abdeslam El Ftouh <abdeslamelftouh@gmail.com>
2026-06-04 14:34:04 +0200

    ADD file soluzioni1.sql

---
+++ file sql/soluzioni1.sql
@@ -0,0 +1,29 @@
+-- Selezionare tutti gli studenti nati nel 1990 (Risultato atteso: 160)
+
+
+
+-- Selezionare tutti i corsi che valgono più di 10 crediti (Risultato atteso: 479)
+
+
+
+-- Selezionare tutti gli studenti che hanno più di 30 anni
+
+
+
+-- Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea (Risultato atteso: 286)
+
+
+
+-- Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14:00) del 20/06/2020 (Risultato atteso: 21)
+
+
+
+-- Selezionare tutti i corsi di laurea magistrale (Risultato atteso: 38)
+
+
+
+-- Calcolare da quanti dipartimenti è composta l'università (Risultato atteso: 12)
+
+
+
+-- Calcolare quanti sono gli insegnanti che non hanno un numero di telefono (Risultato atteso: 50)
